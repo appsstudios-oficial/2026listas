3162687cdb04ca1e2e66623f19c7186428da1155
appsstudios-oficial <appsstudiosarg@gmail.com>
2026-07-12 22:00:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
+import requests
+from flask import Flask, Response, request, redirect
+
+app = Flask(__name__)
+
+# Tu lista real de GitHub (la que tiene los canales reales)
+URL_LISTA_GITHUB = "https://raw.githubusercontent.com/appsstudios-oficial/2026listas/refs/heads/main/full.m3u"
+
 @app.route('/lista.m3u')
 def proteger_lista():
-    # Eliminamos el bloqueo temporalmente para probar
     try:
         respuesta = requests.get(URL_LISTA_GITHUB, timeout=10)
         if respuesta.status_code != 200:
@@ -10,3 +18,7 @@ def proteger_lista():
         
     except Exception as e:
         return "Error interno del servidor", 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
